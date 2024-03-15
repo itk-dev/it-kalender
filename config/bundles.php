7fff7cc093d717818b6e5dfe20262e4fa0d04955
Mikkel Ricky <rimi@aarhus.dk>
2024-03-15 23:28:20 +0100

    Emabled profiler

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
